b0bf69b7f95eebe8698f9a6a5bc05ee8e9e4c46a
JnrLouis <chibuezelouis@gmail.com>
2022-01-17 01:02:21 +0100

    First commit

---
+++ Whitelist/test/test.js
@@ -0,0 +1,40 @@
+const { messagePrefix } = require("@ethersproject/hash");
+const { ethers } = require("hardhat");
+
+async function main() {
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so whitelistContract here is a factory for instances of our Whitelist contract.
+  */
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
+
+  // here we deploy the contract
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // Wait for it to finish deploying
+  await deployedWhitelistContract.deployed();
+
+  // print the address of the deployed contract
+  console.log(
+    "Whitelist Contract Address:",
+    deployedWhitelistContract.address
+  );
+
+  let txn = await deployedWhitelistContract.addAddressToWhitelist();
+  await txn.wait();
+
+  /*let num = await deployedWhitelistContract.getNumAdd();
+  console.log(num);
+
+  let max = await deployedWhitelistContract.checkWhitelisted();
+  console.log(max);*/
+
+
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
